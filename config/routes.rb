b8e215ccf3b6fe8c0da25b6bf75bb92501b0e335
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-07-12 01:07:54 +0530

    Add inventory controller,views and models

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :inventories do
+    resources :inventory_foods, only: [:new, :create, :destroy]
+  end
+  
 
 end
