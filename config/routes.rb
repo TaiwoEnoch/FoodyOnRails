93c7ef6d6a358e0c9fda47496fbe8dc0b09c09ad
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-07-12 11:39:13 +0100

    Add recipe link into home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/public_recipes', to: 'recipes#index', as: 'public_recipes'
 
   # Defines the root path route ("/")
   # root "articles#index"
