066673d8cad93b83bd77486f0deea0daeb9a3674
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-07-13 03:44:17 +0530

    Add specs for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
 
   has_many :inventories
   has_many :foods
   has_many :recipes
-
 end
