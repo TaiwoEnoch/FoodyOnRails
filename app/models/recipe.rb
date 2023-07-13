11b7ccb3d9553d48ebd113fb75243e227aea43a7
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-07-13 19:37:40 +0100

    Add model for recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,9 +3,15 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
+  scope :public_recipes, -> { where(public: true) }
+
   validates :name, presence: true
   validates :cooking_time, presence: true
   validates :preparation_time, presence: true
   validates :description, presence: true
   validates_inclusion_of :public, in: [true, false]
+
+  def total_price
+    foods.map { |f| f.price.to_i * f.recipe_foods.find_by(recipe_id: id).quantity.to_i }.sum
+  end
 end
