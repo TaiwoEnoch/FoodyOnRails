9ef00e84d1ef8e0fd4681f3960169bfdb470b605
karan jain <karanjain2212@gmail.com>
2023-07-14 21:42:01 +0530

    inventory food form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  #resources :recipes
+  
   devise_for :users
-  # get 'home/index'
+
   authenticated :user do
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
   resources :foods, only: %i[index new create destroy]
   resources :recipes, only: %i[index show new create destroy update] do
     member do
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
     resources :recipe_foods, only: [:new, :create,  :update,  :destroy],  except: [:show]
   end
   resources :public_recipes, only: %i[index show]
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :inventories do
+  resources :general_shopping_list, only: [:index ,:show]
+  resources :inventories, only: %i[index new create show destroy] do
     resources :inventory_foods, only: [:new, :create, :destroy]
+    get 'shopping_list', on: :member
   end
- end
+end
 
 end
