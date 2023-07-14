d3f66f84d28d3ba05d1bdc71608a35e488470f7c
karan jain <karanjain2212@gmail.com>
2023-07-14 14:42:37 +0530

    added food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,8 @@
 class Food < ApplicationRecord
   has_many :inventory_foods
   has_many :inventories, through: :inventory_foods
-  has_many :recipe_foods
+  belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true
