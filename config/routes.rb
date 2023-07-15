6528e9f2bcfd531e0b5968caf549cc0cadf9f964
karan jain <karanjain2212@gmail.com>
2023-07-15 19:56:39 +0530

    add ingredient

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
  
   authenticated :user do
-  root 'home#index'
- 
-  resources :foods, only: %i[index new create destroy]
-  resources :recipes, only: %i[index show new create destroy update] do
-    member do
-      post 'toggle_public'
+    root 'recipes#index', as: :authenticated_root
+
+    resources :foods, only: %i[index new create destroy]
+    resources :recipes, only: %i[index show new create destroy update] do
+      member do
+        post 'toggle_public'
+     
+      end
+
+      resources :recipe_foods, only: %i[new create destroy]
     end
-    resources :foods, only: [:new, :create]
-    resources :recipe_foods, only: [:new]
-  end
   resources :public_recipes, only: %i[index show]
   resources :general_shopping_list, only: [:index ,:show]
   resources :inventories, only: %i[index new create show destroy] do
     resources :inventory_foods, only: [:new, :create, :destroy]
   end
 end
+root 'home#index'
 get 'shopping_list', to: 'recipes#shopping_list', as: 'shopping_list'
 end
