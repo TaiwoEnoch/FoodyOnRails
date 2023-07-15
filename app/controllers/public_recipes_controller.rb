14e15b715f48427c74e753c201b9fc9826479422
karan jain <karanjain2212@gmail.com>
2023-07-16 03:47:00 +0530

    public recipe

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,11 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.public_recipes.order(created_at: :desc)
-    @recipe_ingredients = RecipeFood.all
-  end
-
-  def show
-    @recipe = Recipe.public_recipes.find(params[:id])
-    @ingredients = @recipe&.recipe_foods
+    @recipes = Recipe.includes(:foods).where(public: true).order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
- 
+
   authenticated :user do
     root 'recipes#index', as: :authenticated_root
 
     resources :foods, only: %i[index new create destroy]
     resources :recipes, only: %i[index show new create destroy update] do
       member do
-        post 'toggle_public'
-     
+        patch 'toggle'
       end
-
       resources :recipe_foods, only: %i[new create destroy]
     end
-  resources :public_recipes, only: %i[index show]
-  resources :general_shopping_list, only: [:index ,:show]
-  resources :inventories, only: %i[index new create show destroy] do
-    resources :inventory_foods, only: [:new, :create, :destroy]
+
+    resources :inventories, only: %i[index new create show destroy] do
+      resources :inventory_foods, only: %i[new create destroy]
+    end
   end
-end
-root 'home#index'
-get 'shopping_list', to: 'recipes#shopping_list', as: 'shopping_list'
+
+  resources :public_recipes, only: %i[index show]
+  resources :general_shopping_list, only: [:index, :show]
+
+  root 'home#index'
+  get 'shopping_list', to: 'recipes#shopping_list', as: 'shopping_list'
 end
