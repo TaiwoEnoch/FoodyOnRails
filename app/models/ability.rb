c1880de2b1c9b9d2afb9c530842b2d0d10919af1
karan jain <karanjain2212@gmail.com>
2023-07-15 22:32:19 +0530

    cancan gem

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+    include CanCan::Ability
+  
+    def initialize(user)
+      return unless user.present?
+  
+      can :read, Inventory
+      can :read, Recipe
+      can :manage, Inventory, user_id: user.id
+      can :manage, Recipe, user_id: user.id
+    end
+  end
+  
